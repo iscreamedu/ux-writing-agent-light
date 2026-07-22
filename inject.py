a8351175c1549d5b9a48f17f5683d326bee84f1c
이은지C(디자인팀/eunjilee) <eunjilee@i-screamedu.co.kr>
2026-07-22 17:03:02 +0900

    feat: replace Gemini spell check with embedded SPELL_DATA rule-based checker

--- inject.py
+++ inject.py
@@ -13,5 +13,11 @@ with open('guide-data.json', 'r', encoding='utf-8') as f:
 new_block = '/* __DATA_START__ */\nconst EMBEDDED_DATA = ' + guide + '\n/* __DATA_END__ */'
 c = re.sub(r'/\* __DATA_START__ \*/.*?/\* __DATA_END__ \*/', new_block, c, flags=re.DOTALL)
 
+with open('spell-data.json', 'r', encoding='utf-8') as f:
+    spell = f.read()
+
+new_spell_block = '/* __SPELL_START__ */\nconst SPELL_DATA = ' + spell + '\n/* __SPELL_END__ */'
+c = re.sub(r'/\* __SPELL_START__ \*/.*?/\* __SPELL_END__ \*/', new_spell_block, c, flags=re.DOTALL)
+
 with open('dist/index.html', 'w', encoding='utf-8') as f:
     f.write(c)
